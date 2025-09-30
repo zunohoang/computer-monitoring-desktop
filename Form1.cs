8431432f8991b068e525b217e8dd8063ad8c652b
Nam Khuc <khucphuongnam2005@gmail.com>
2025-09-30 10:11:36 +0700

    Apply .gitignore and add solution files

---
+++ Form1.cs
@@ -0,0 +1,15 @@
+namespace computer_monitoring_desktop
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
